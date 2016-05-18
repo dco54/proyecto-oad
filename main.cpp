17b4463482601ca6b429c20bc0cbd6eebdf289db
haydeluzmilahc <haydeluzmilahc@users.noreply.github.com>
2016-05-17 20:52:54 -0500

    Update and rename hilos to main.cpp

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qcustomplot.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    graficadora = new tabulador;
+    graficadora->range_negative=range_x_ne;
+    graficadora->range_positive=range_x;
+    // Note: we could have also just called customPlot->rescaleAxes(); instead
+    // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+    qDebug() << "Valor RANGO X NEGATIVO:" <<this->range_x_ne<<"Valor RANGO X POSITIVO:"<<this->range_x;
+}
+
+MainWindow::~MainWindow()
+{
+    delete graficadora;
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QVector<double> x(tam_x), y(tam_y); // initialize with entries 0..100
+    qDebug() << "TABULADOR DE LA GRAFICADORA";
+    graficadora->reconoce_funcion(ui->line_edit_funcion->text().toStdString());
+    if(graficadora->getEstado()==false){
+    for (int i=0; i<1001; ++i)
+    {
+        if((i/10.0)-50>range_x)
+            break;
+        if(((i/10.0)-50>=range_x_ne)){
+                x[i] = *(graficadora->arrays_x + i);
+                y[i] = *(graficadora->arrays_y + i);
+                qDebug() << "Valor eje x:" <<x[i]<<"Valor eje y :"<<y[i];
+        }
+    }
+    // create graph and assign data to it:
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(x,y);
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(range_x_ne, range_x);
+    ui->customPlot->yAxis->setRange(range_x_ne, range_x);
+    ui->customPlot->replot();
+    qDebug() << "FIN DEL TABULADOR";
+    }
+}
+
+
+void MainWindow::on_grabar_clicked()
+{
+    bool ok;
+    double valor_range_x=ui->lineX->text().toDouble(&ok);
+    double valor_range_x_ne=ui->lineX_ne->text().toDouble(&ok);
+    this->range_x=valor_range_x;
+    this->range_x_ne=valor_range_x_ne;
+    graficadora->range_negative=range_x_ne;
+    graficadora->range_positive=range_x;
+    qDebug() << "Valores seteados";
+    qDebug() << "Valor RANGO X NEGATIVO:" <<this->range_x_ne<<"Valor RANGO X POSITIVO:"<<this->range_x;
+}
